b594b8e43498acbc94772730743021376c22fb40
agdiaz4 <96065916+agdiaz4@users.noreply.github.com>
2021-12-16 08:22:00 -0600

    Added extension

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
+// Functions that gives info to the user.
 function splashSolution (Splash: string, Solution: string) {
     return "SORRY, IT WAS " + solution
 }
+// Functions the whole game when played.
 function checkAnswer () {
     answer = game.askForString("I'M THINKING OF A LETTER...", 1)
     if (answer == solution) {
         game.over(true, effects.confetti)
     } else {
         info.changeLifeBy(-1)
+        // Conditionals that give many outcomes
         if (info.life() > 0) {
             hint = "HIGHER"
             if (answer > solution) {
@@ -18,9 +21,11 @@ function checkAnswer () {
         }
     }
 }
+// Function that gives a hint for help.
 function buildHint (Splash: string, Hint: string) {
     return "SORRY, TRY " + hint
 }
+// Function that set's life for the user.
 function lifeSet () {
     info.setLife(3)
     solution = String.fromCharCode(randint(97, 122))
@@ -28,13 +33,19 @@ function lifeSet () {
 let hint = ""
 let answer = ""
 let solution = ""
-scene.setBackgroundColor(2)
+// Sets a good scenery instead of dull one
+scene.setBackgroundColor(13)
+// Question that ask for you name.
 let input2 = game.askForString("What is your name?")
+// Gives the user a welcoming.
 game.splash("Hello " + input2 + "! " + "Welcome " + "to " + "Guess " + "the " + "Letter" + "!")
 lifeSet()
+// Continues the game based on lives.
 while (info.life() > 0) {
     checkAnswer()
 }
+// Block identify if you have 0 lives
+// 
 while (false) {
     game.over(false)
 }
